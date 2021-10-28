c3d878e432d991aa41bffe4cddd3c5fcfd8646e4
Andrii Hladkyi <hladkiy1500@gmail.com>
2021-10-28 12:57:42 +0300

    - improvements

--- src/test/java/exercises/EmailValidator.java
+++ src/test/java/exercises/EmailValidator.java
@@ -3,15 +3,18 @@ package exercises;
 import java.util.Scanner;
 
 public class EmailValidator {
+
+    public static final char AT = '@';
+    public static final char DOT = '.';
+
     public static void main(String[] args) {
         System.out.println("Please enter email address:");
         String email = EmailValidator.enterEmailAddress();
-        boolean endDot = EmailValidator.checkingEndDot(email);
-        int atCount = EmailValidator.countingOfAt(email);
-        boolean checkPrefix = EmailValidator.checkingPrefix(email);
-        int countOfDotAfterAt = EmailValidator.countingDotAfterAt(email);
+        boolean endDot = EmailValidator.isEndsDot(email);
+        int atCount = EmailValidator.countOfAt(email, AT);
+        boolean checkPrefix = EmailValidator.isPrefixPresent(email);
+        int countOfDotAfterAt = EmailValidator.countDotAfterAt(email, DOT);
         EmailValidator.validationMessage(endDot, atCount, checkPrefix, countOfDotAfterAt);
-
     }
 
     public static String enterEmailAddress() {
@@ -19,50 +22,52 @@ public class EmailValidator {
         return scanner.next();
     }
 
-    public static boolean checkingEndDot(String email) {
-        boolean checkEndDot;
-        checkEndDot = email.endsWith(".");
+    public static boolean isEndsDot(String email) {
+        boolean checkEndDot = email.endsWith(String.valueOf(DOT));
         if (checkEndDot) {
             System.out.println("Warning: email ends with dot!");
         }
         return checkEndDot;
     }
 
-    public static int countingOfAt(String email) {
-        int countOfAt = 0;
+    public static int countCharacters(String email, char character) {
+        int count = 0;
         for (int i = 0; i < email.length(); i++) {
-            if (email.charAt(i) == '@') {
-                countOfAt++;
-            }
+            if (email.charAt(i) == character)
+                count++;
         }
-        if (countOfAt != 1) {
-            System.out.println("Warning: only one @ allowed!");
+        return count;
+    }
+
+    public static int countOfAt(String email, char character) {
+        int countOfAt = 0;
+        boolean isAtPresent = email.contains(String.valueOf(AT));
+        if (!isAtPresent) {
+            System.out.println("AT is not present in email!");
+        } else {
+            countOfAt = countCharacters(email, character);
+            if (countOfAt != 1) {
+                System.out.println("Warning: mail address must contain one @!");
+            }
         }
         return countOfAt;
     }
 
-    public static boolean checkingPrefix(String email) {
-        String[] dividedEmail = email.split("@");
-        boolean checkPrefix;
-        checkPrefix = dividedEmail[0].isEmpty();
+    public static int countDotAfterAt(String email, char character) {
+        String buffering = email.substring(email.indexOf(AT) + 1);
+        int countDotAfterAt = countCharacters(buffering, character);
+        if (countDotAfterAt != 1) {
+            System.out.println("Warning: suffix part of email must contain one dot!");
+        }
+        return countDotAfterAt;
+    }
+
+    public static boolean isPrefixPresent(String email) {
+        boolean checkPrefix = email.split(String.valueOf(AT))[0].isEmpty();
         if (checkPrefix) {
             System.out.println("Warning: email prefix is empty!");
         }
         return checkPrefix;
-    }
-
-    public static int countingDotAfterAt(String email) {
-        String buffering = email.substring(email.indexOf('@') + 1);
-        int len = buffering.length();
-        int countOfDotAfterAt = 0;
-        for (int i = 0; i < len; i++) {
-            if (buffering.charAt(i) == '.')
-                countOfDotAfterAt++;
-        }
-        if (countOfDotAfterAt != 1) {
-            System.out.println("Warning: suffix part of email must contain one dot!");
-        }
-        return countOfDotAfterAt;
     }
 
     public static void validationMessage(boolean checkEndDot, int countOfAt, boolean checkPrefix, int countOfDotAfterAt) {
