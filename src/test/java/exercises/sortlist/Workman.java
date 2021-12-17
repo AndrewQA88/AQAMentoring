757f7c460155a42db5ea8e74257f874af9d59b15
Andrii Hladkyi <hladkiy1500@gmail.com>
2021-12-18 00:11:14 +0200

    - sort list

---
+++ src/test/java/exercises/sortlist/Workman.java
@@ -0,0 +1,69 @@
+package exercises.sortlist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Workman implements Comparable<Workman> {
+
+    int experience;
+    int salary;
+    int age;
+
+    public Workman(int experience, int salary, int age) {
+        this.experience = experience;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(Workman o) {
+        return this.age - o.getAge();
+    }
+
+    @Override
+    public String toString() {
+        return "Workman{" +
+                "experience=" + experience +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        List<Workman> employees = new ArrayList<>();
+        employees.add(new Workman(3, 2000, 32));
+        employees.add(new Workman(1, 700, 22));
+        employees.add(new Workman(6, 10000, 37));
+        employees.add(new Workman(7, 3000, 29));
+
+        Collections.sort(employees);
+        System.out.println("Comparable: sort by age.");
+        System.out.println(employees);
+
+        System.out.println("Comparator: sort by experience.");
+        Comparator<Workman> experience = new ExperienceComparator();
+        Collections.sort(employees, experience);
+        System.out.println(employees);
+
+        System.out.println("Comparator: sort by salary.");
+        Comparator<Workman> salary = new SalaryComparator();
+        Collections.sort(employees, salary);
+        System.out.println(employees);
+
+    }
+}
